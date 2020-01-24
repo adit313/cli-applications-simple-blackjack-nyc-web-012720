1ca515f84400df6fba639ffedd6dda81bfc87bce
Adit P Patel <app2116@gmail.com>
2020-01-24 20:21:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -39,7 +39,7 @@ def hit?(current_total)
   prompt_user
   input = get_user_input
   while !(input == 'h' || input == 's')
-      invalid_command
+      input = invalid_command
   end
   return current_total if input == 's'
   return (current_total+deal_card) if input == 'h'
